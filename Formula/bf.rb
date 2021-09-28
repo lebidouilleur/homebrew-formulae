9350357b48cb8ad4b74884115969ea93502d4318
le Bidouilleur <lebidouilleur@icloud.com>
2021-09-28 18:39:16 +0200

    Add a pre-stable version

--- Formula/bf.rb
+++ Formula/bf.rb
@@ -4,6 +4,10 @@ class Bf < Formula
   homepage "https://github.com/lebidouilleur/bf"
 
 
+  stable do
+    url    "https://github.com/lebidouilleur/bf/archive/refs/tags/v0.1.tar.gz"
+    sha256 "7ffca09a2a1b6a75d710a98defd264d735491769cf790f4bcdcbd2582a600313"
+  end
 
   head do
     url "https://github.com/lebidouilleur/bf.git"

def515907a39f789b5f1d7ba99177c3dfdee90dd
le Bidouilleur <lebidouilleur@icloud.com>
2021-10-27 20:41:28 +0200

    Update dependency

--- Formula/cbz2pdf.rb
+++ Formula/cbz2pdf.rb
@@ -6,10 +6,10 @@ class Cbz2pdf < Formula
 
   stable do
     url     "https://github.com/lebidouilleur/cbz2pdf/archive/refs/tags/v1.0.0.tar.gz"
-    sha256  "fba4f9dadad4a6532c6ff78ecd43bdaca17cb4d8feface6fd157e41400841151"
+    sha256  "631c1728cfbebed80d181961a853de8bd89afe2e67e6b6e1cbc5b67dda6d2abb"
     version "1.0.0"
 
-    depends_on "unrar" => :recommended
+    depends_on "libarchive" => :recommended
   end
 
 
@@ -17,7 +17,7 @@ class Cbz2pdf < Formula
   head do
     url "https://github.com/lebidouilleur/cbz2pdf"
 
-    depends_on "unrar" => :recommended
+    depends_on "libarchive" => :recommended
   end
 
 

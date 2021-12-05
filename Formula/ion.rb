96cc0742965ece82a13643da85d607383e0d79fa
le Bidouilleur <lebidouilleur@icloud.com>
2021-12-05 15:50:49 +0100

    Fix typo in canvas

--- Formula/ion.rb
+++ Formula/ion.rb
@@ -4,6 +4,15 @@ class Ion < Formula
   homepage "https://gitlab.redox-os.org/redox-os/ion"
 
 
+
+  stable do
+    url "https://gitlab.redox-os.org/redox-os/ion/-/archive/1.0.5/ion-1.0.5.tar.gz"
+    sha256 "7c7f696060a82669cb9c73cf0ec1d07d4ffefbfd54fc60acad0179ca3f46716d"
+
+    depends_on "rust" => :build
+  end
+
+
   head do
     url "https://gitlab.redox-os.org/redox-os/ion.git"
 
@@ -24,9 +33,8 @@ class Ion < Formula
           #{prefix}
 
       To make Redox's Ion your default shell:
-        echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells
+        echo '/usr/local/bin/ion' | sudo tee -a /etc/shells
         chsh -s /usr/local/bin/ion $USER
     EOS
   end
-
 end
